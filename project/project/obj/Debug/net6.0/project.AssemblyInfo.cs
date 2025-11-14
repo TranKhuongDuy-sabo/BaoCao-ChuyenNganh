f7b9378ed7f32857a784ff5789629112dc120edd
TranKhuongDuy-sabo <duyt2296@gmail.com>
2025-11-14 10:03:52 +0700

    update layout product

--- project/project/obj/Debug/net6.0/project.AssemblyInfo.cs
+++ project/project/obj/Debug/net6.0/project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af2667c1790b5b32aaa451af3b4246a072b22fea")]
 [assembly: System.Reflection.AssemblyProductAttribute("project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
