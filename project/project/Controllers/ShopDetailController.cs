5da83ee2942f26532e5bb4679c7018eb072d14a7
TranKhuongDuy-sabo <duyt2296@gmail.com>
2025-11-16 01:00:31 +0700

    update và them file .doc

---
+++ project/project/Controllers/ShopDetailController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace project.Controllers
+{
+    public class ShopDetailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
