3d4c3de61b2ce7a00e40aa9973ca864a900fc91b
TranKhuongDuy-sabo <duyt2296@gmail.com>
2025-12-05 17:08:56 +0700

    update

--- project/project/Areas/Admin/Controllers/BrandController.cs
+++ project/project/Areas/Admin/Controllers/BrandController.cs
@@ -31,5 +31,79 @@ namespace project.Areas.Admin.Controllers
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult xoa(int id)
+        {
+            var brand = db.Brands.Find(id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                db.Brands.Remove(brand);
+                db.SaveChanges();
+                TempData["Success"] = "Đã xóa thương hiệu thành công!";
+            }
+            catch (System.Exception)
+            {
+                TempData["Error"] = "Không thể xóa! Thương hiệu này đang chứa sản phẩm.";
+                return View("formXoa", brand);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult formXoa(int id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var brand = db.Brands.Find(id);
+            if (brand == null)
+            {
+                return NotFound();
+            }
+
+            return View("formXoa", brand);
+        }
+
+        [HttpPost]
+        public IActionResult sua(Brand brand)
+        {
+            if (ModelState.IsValid)
+            {
+                var br = db.Brands.Find(brand.BrandId);
+                if (br != null)
+                {
+                    br.BrandName = brand.BrandName;
+                    br.Origin = brand.Origin;
+                    db.SaveChanges();
+                    TempData["Success"] = "Đã cập nhật thương hiệu thành công!";
+                }
+                else
+                {
+                    TempData["Error"] = "Không tìm thấy thương hiệu để cập nhật!";
+                }
+                return RedirectToAction("Index");
+            }
+            return View("formSua", brand);
+        }
+
+        [HttpGet]
+        public IActionResult formSua(int id)
+        {
+            var category = db.Brands.Find(id);
+            if (category == null)
+            {
+                TempData["Error"] = "Không tìm thấy thương hiệu cần sửa!";
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
     }
 }
--- project/project/obj/Debug/net8.0/project.AssemblyInfo.cs
+++ project/project/obj/Debug/net8.0/project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a798fa3277fb0afd7a9300c2e75cf73ec0bb26db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4aa8046dd3e032d1b3ea6a79bf602074c9949da")]
 [assembly: System.Reflection.AssemblyProductAttribute("project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
